e8757bb7a081900c2f3af59e9fe20d421f12ef7b
exoream <shiigetsu1@gmail.com>
2025-01-26 12:46:37 +0800

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const { initializeBot } = require("./discord-bot")
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 let botInitialized = false
 
